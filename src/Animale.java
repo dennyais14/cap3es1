692f82f23b915a7cff52bc510be4567b31b6d354
danieleocchi <dennyais146@gmail.com>
2024-01-27 12:19:43 +0100

    first version

---
+++ src/Animale.java
@@ -0,0 +1,10 @@
+public class Animale {
+    public void faiIlVerso(){
+        System.out.println("miao");
+    }
+
+    @Override
+    public String toString() {
+        return "Animale{}";
+    }
+}
